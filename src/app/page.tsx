cc0cabe7e83c811c98964f61ffa5d1fbf0a4fc96
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-10-15 14:23:18 -0300

    add tabs and use framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SettingTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingTabs />
     </>
   )
 }
