313213cba14514820238247e970fa37d09158014
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-10-15 11:33:16 -0300

    base structure and sidebar

---
+++ src/components/Sidebar/MainNavigation/index.tsx
@@ -0,0 +1,27 @@
+import {
+  BarChart2,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { NavItem } from './NavItem'
+
+export function MainNavigation() {
+  return (
+    <nav className="space-y-0.5">
+      <NavItem title="Home" icon={Home} />
+      <NavItem title="Dashboard" icon={BarChart2} />
+      <NavItem title="Projects" icon={SquareStack} />
+      <NavItem title="Tasks" icon={CheckSquare} />
+      <NavItem title="Reporting" icon={Flag} />
+      <NavItem title="Users" icon={Users} />
+    </nav>
+  )
+}
+
+/**
+ * space-y-0.5
+ * Adiciona um espaco vertical de 2px entre todos os items
+ */
